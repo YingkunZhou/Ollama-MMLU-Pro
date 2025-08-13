779cf412505be7702ef1507410e8cb38888dd765
YingkunZhou <zhouyingkun19@163.com>
2025-08-13 12:57:31 +0800

    support QK_T alternative, e.g. 128, 256

--- transpose.c
+++ transpose.c
@@ -11,13 +11,14 @@ static inline uint8_t transpose_bits(
     return result;
 }
 
-void do_my_transpose(uint8_t *dst, uint8_t *src, int nx, int ny) {
-    for (int ix = 0; ix < nx; ix += 128) {
-        int offset = 0;
+
+void do_my_transpose(uint8_t *dst, uint8_t *src, int nx, int ny, int stride) {
+    int offset = 0;
+    for (int ix = 0; ix < nx; ix += stride) {
 
         // loop for d (128*2)
-        for (int i = 0; i < 256; i++) {
-            dst[ix * ny + offset++] = src[(ix + i/2) * ny + i%2];
+        for (int i = 0; i < stride*2; i++) {
+            dst[offset++] = src[(ix + i/2) * ny + i%2];
         }
 
         // loop for supergroup
@@ -25,28 +26,30 @@ void do_my_transpose(uint8_t *dst, uint8_t *src, int nx, int ny) {
             for (int k = 0; k < 8; k++) {
 
                 // extra (1-bit)
-                for (int i = 0; i < 128; i += 8) {
-                    dst[ix * ny + offset++] = transpose_bits(src, ix+i, base, k, ny, 1, 1);
+                for (int i = 0; i < stride; i += 8) {
+                    dst[offset++] = transpose_bits(src, ix+i, base, k, ny, 1, 1);
                 }
 
                 // scale_h (1-bit)
-                for (int i = 0; i < 128; i += 8) {
-                    dst[ix * ny + offset++] = transpose_bits(src, ix+i, base+1, k, ny, 1, 1);
+                for (int i = 0; i < stride; i += 8) {
+                    dst[offset++] = transpose_bits(src, ix+i, base+1, k, ny, 1, 1);
                 }
 
                 // scale_l (4-bit)
-                for (int i = 0; i < 128; i += 8) {
+                for (int i = 0; i < stride; i += 8) {
                     for (int j = 0; j < 4; j++) {
-                        dst[ix * ny + offset++] = transpose_bits(src, ix+i+j, base+2+k/2, k%2, ny, 4, 4);
+                        dst[offset++] = transpose_bits(src, ix+i+j, base+2+k/2, k%2, ny, 4, 4);
                     }
                 }
 
                 // qs (2-bit)
                 for (int i = 0; i < 32; i++) {
-                    for (int jjj = 0; jjj < 2; jjj++) {
-                        for (int jj = jjj; jj < 8; jj += 2) {
-                            for (int j = 0; j < 4; j++) {
-                                dst[ix * ny + offset++] = transpose_bits(src, ix+4*jj+j, base+6+i+32*(k/4), k%4, ny, 32, 2);
+                    for (int ii = 0; ii < stride/128; ii++) {
+                        for (int jjj = 0; jjj < 2; jjj++) {
+                            for (int jj = jjj; jj < 8; jj += 2) {
+                                for (int j = 0; j < 4; j++) {
+                                    dst[offset++] = transpose_bits(src, ix+128*ii+4*jj+j, base+6+i+32*(k/4), k%4, ny, 32, 2);
+                                }
                             }
                         }
                     }
