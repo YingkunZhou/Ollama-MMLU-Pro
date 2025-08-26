324727f97e82919ad307dfa2741c13906c28b1c6
YingkunZhou <zhouyingkun19@163.com>
2025-08-26 23:59:46 +0800

    refactor playgguf and quickstart ipynb logic

--- .config/ryzen-7700X-freq.sh
+++ .config/ryzen-7700X-freq.sh
@@ -1,8 +1,8 @@
 echo -1 > /proc/sys/kernel/perf_event_paranoid
-# trun off hyper thread in bios
+echo off | sudo tee /sys/devices/system/cpu/smt/control
 for i in $(seq 0 7);
 do
-    echo 800000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_min_freq
+    echo 400000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_min_freq
     echo 4500000 > /sys/devices/system/cpu/cpufreq/policy$i/scaling_max_freq
     echo powersave > /sys/devices/system/cpu/cpufreq/policy$i/scaling_governor
     echo balance_performance > /sys/devices/system/cpu/cpufreq/policy$i/energy_performance_preference
